612b4a15f6f3e46b9a32bbb05647c08fec582158
k1rowashere <Kyrollos Youssef@ABOOOKH>
2022-08-15 16:23:43 +0200

    bad url in breadcrumb

--- nextjs/src/components/StoreFront/Breadcrumb.tsx
+++ nextjs/src/components/StoreFront/Breadcrumb.tsx
@@ -2,14 +2,14 @@ import Link from 'next/link';
 import React from 'react';
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
 
-export default function _Breadcrumb(props: {activePath: string[], pid?: string, [x: string]: any}) {
+export default function _Breadcrumb(props: { activePath: string[], pid?: string, [x: string]: any }) {
     const { activePath, pid, ...rest } = props;
     return (
-        <Breadcrumb listProps={{className: 'my-auto', style:{'--bs-breadcrumb-item-padding-x': '0.3rem'} as React.CSSProperties}} {...rest}>
+        <Breadcrumb listProps={{ className: 'my-auto', style: { '--bs-breadcrumb-item-padding-x': '0.3rem' } as React.CSSProperties }} {...rest}>
             <Link href='/store' scroll={false} passHref><Breadcrumb.Item className='h4' key='store'>STORE</Breadcrumb.Item></Link>
             {activePath.map((page, index) => {
                 return (
-                    <Link href={`/store/${activePath.slice(0, index + 1).join('/')}`} key={page} passHref>
+                    <Link href={`/store/${activePath.slice(0, index + 1).join('/').toLowerCase().replace(/\s/g, '-')}`} key={page} passHref>
                         <Breadcrumb.Item className='h4'>
                             {page.replaceAll('-', ' ').toUpperCase()}
                         </Breadcrumb.Item>
