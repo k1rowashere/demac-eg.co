558a0092525074f15f8d063ee528525ece177454
k1rowashere <Kyrollos Youssef@ABOOOKH>
2022-08-24 19:18:49 +0200

    optimized category items

--- nextjs/src/components/StoreFront/Categories/CategoryItem.tsx
+++ nextjs/src/components/StoreFront/Categories/CategoryItem.tsx
@@ -4,11 +4,13 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import Collapse from 'react-bootstrap/Collapse';
 
 //helper functions
-function arrayEquals(a: any[], b: any[]) {
+function arrayEquals(a?: any[], b?: any[]) {
+    if (a === undefined || b === undefined) return false;
     return a.length === b.length && a.every((val, index) => val === b[index]);
 }
 
-function isPartOfPath(thisPath: any[], activePath: any[]) {
+function isPartOfPath(thisPath?: any[], activePath?: any[]) {
+    if (thisPath === undefined || activePath === undefined) return false;
     return thisPath.length < activePath.length && thisPath.every((el, index) => activePath[index] === el)
 }
 
@@ -16,26 +18,27 @@ type categories = {
     [x: string]: categories | {};
 }
 
-type CategoryItem = { prevPath: string[], name: string, level: number, currCategories: categories, activePath: string[] }
+type CategoryItem = { prevPath: string[], name: string, level: number, currCategories: categories, activePath?: string[] }
 
 export default function CategoryItem({ prevPath, name, level, currCategories, activePath }: CategoryItem) {
-    const thisPath = prevPath.concat([name.replaceAll(/\s/g, '-').toLowerCase()]);
-    const hasChildren = Object.keys(currCategories).length === 0;
+    const thisPath = useMemo(() => prevPath.concat([name.replaceAll(/\s/g, '-').toLowerCase()]), []);
     const [expanded, setExpanded] = useState(level < 1 || isPartOfPath(thisPath, activePath));
-    
+
     return useMemo(() => {
-        const chevronHandle = () => {setExpanded(!expanded)}
         const icons = ['bi bi-chevron-right', 'bi bi-chevron-down'];
+        const hasChildren = Object.keys(currCategories).length === 0;
+        const chevronHandle = () => { setExpanded(!expanded) }
         return <>
             <ListGroup.Item as="li" className='text-truncate' active={arrayEquals(activePath, thisPath)} key={thisPath.toString()}>
                 {hasChildren
-                    ? <i className='bi bi-dash text-muted' />
-                    : <i role='button' className={icons[Number(expanded)]} onClick={chevronHandle} />
-                }
-
-                {!hasChildren
-                    ? <a href='#;' tabIndex={0} onClick={chevronHandle}>{name}</a>
-                    : <Link href={thisPath.join('/')} passHref scroll={false}><a>{name}</a></Link>
+                    ? <>
+                        <i className='bi bi-dash text-muted' />
+                        <Link href={thisPath.join('/')} passHref scroll={false}><a>{name}</a></Link>
+                    </>
+                    : <>
+                        <i role='button' className={icons[Number(expanded)]} onClick={chevronHandle} />
+                        <a href='#;' tabIndex={0} onClick={chevronHandle}>{name}</a>
+                    </>
                 }
                 {/* recurse over all children */}
                 <Collapse in={expanded}>
@@ -55,5 +58,5 @@ export default function CategoryItem({ prevPath, name, level, currCategories, ac
                 </Collapse>
             </ListGroup.Item>
         </>
-    }, [expanded, activePath, currCategories, hasChildren, level, name, thisPath]);
+    }, [expanded, thisPath, name, level, currCategories, activePath]);
 }
